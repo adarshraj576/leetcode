8553b6d0b55ae00beb6df70f57edc8ef86c4025c
Adarsh Raj <adarshraj576@gmail.com>
2025-01-21 20:26:16 +0530

    Add Matrix Search

---
+++ search_a_2d_matrix.cpp
@@ -0,0 +1,19 @@
+// Leetcode : https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int left = 0;
+        int right = rows * cols - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / cols][mid % cols];
+            if (midValue == target) return true;
+            if (midValue < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+};
