17f76d33b6279305bc4917c42eddc4cdf5e61ab7
Adarsh Raj <adarshraj576@gmail.com>
2025-01-24 18:38:10 +0530

    Add First Bad Version

---
+++ first_bad_version.cpp
@@ -0,0 +1,19 @@
+// Leetcode : https://leetcode.com/problems/first-bad-version/
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(isBadVersion(mid)){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
