75f45d630292f002634f857c0a77d074882ab815
Adarsh Raj <adarshraj576@gmail.com>
2025-01-28 19:40:48 +0530

    Improvise Binary Tree Right Side View

--- binary_tree_right_side_view.cpp
+++ binary_tree_right_side_view.cpp
@@ -1,42 +1,22 @@
-// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+// Leetcode : https://leetcode.com/problems/binary-tree-right-side-view/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     
-    void helper(TreeNode* root, vector<int> &ans, int &level, int height){
-        if(!root){
-            return;
+    void helper(TreeNode* node, int h, vector<int>& ans){
+        if(!node) return;
+        if(ans.size() < h){
+            ans.push_back(node->val);
         }
-        
-        if(level < height){
-            ans.push_back(root->val);
-            level=height;
-        }
-        
-        helper(root->right, ans, level, height+1);
-        helper(root->left, ans, level, height+1);
-        
-        
+        helper(node->right, h+1, ans);
+        helper(node->left, h+1, ans);
     }
-    
-    
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        int level = 0;
-        
-        helper(root, ans, level, 1);
-        
+        if(!root) return ans;
+        ans.push_back(root->val);
+        int height = 1;
+        helper(root, height, ans);
         return ans;
     }
 };
